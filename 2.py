ad2e9c66b7f35039970c28f4482bd7cfa1c33dfe
vkmishra244 <vkmishra244@bit.ac.in>
2025-08-28 14:04:27 +0530

    if else

---
+++ 2.py
@@ -0,0 +1,6 @@
+a=int(input("enter n  "))
+
+if a%2==0:
+    print("even number")
+else:
+    print("odd number")
---
+++ 3.py
@@ -0,0 +1,5 @@
+a=int(input("enter n "))
+if a>0:
+    print("positive number")
+else:
+    print("negative number")
